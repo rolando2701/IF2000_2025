40aecb7b4bad01b7a9f21d1074516f827d485b3d
rolando2701@hotmail.com <rolando2701@hotmail.com>
2025-08-14 11:21:02 -0600

    Mi Primer Commit en Java

---
+++ IF2000_IntroduccionProgra/src/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author rolan
+ */
+public class IF2000_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Data types
+        //Python:
+        //print('Hola Mundo')
+        
+        /*
+        Tipos de datos numéricos:
+        int, long, short, float, double
+        numeric, money, decimal
+        
+        Manejo de fechas:
+        Date
+        
+        Cadenas de caracteres:
+        String,char
+        
+        Objectos:
+        Object
+        
+        Data Structures:
+        Arrays or Vectors: int[] x;
+                           int[][] y;
+        
+        True/False:
+        boolean 
+        
+        */
+        
+        
+        
+        
+    }
+    
+}
